8ee5e44563b8944f2a452836fd6972465155b279
nahid7g <nahid.h.kiran@gmail.com>
2026-07-16 19:50:40 +0600

    Search filter added

--- src/app/modules/post/post.controller.ts
+++ src/app/modules/post/post.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { postService } from "./post.service";
+import { PostStatus } from "../../../../generated/prisma/enums";
 
 const createPost = async (req: Request, res: Response) => {
   try {
@@ -27,7 +28,19 @@ const createPost = async (req: Request, res: Response) => {
 
 const getAllPost = async (req: Request, res: Response) => {
   try {
-    const result = await postService.getAllPost();
+    const search = req.query.search as string | undefined;
+    const tags = req.query.tags ? req.query.tags.toString().split(",") : [];
+    const isFeatured = req.query.isFeatured === "true";
+    const status = req.query.status as PostStatus | undefined;
+    const authorId = req.query.authorId as string | undefined;
+
+    const result = await postService.getAllPost({
+      search,
+      tags,
+      isFeatured,
+      status,
+      authorId,
+    });
     res.status(201).json({
       success: true,
       message: "Post retrived successfully",
--- src/app/modules/post/post.service.ts
+++ src/app/modules/post/post.service.ts
@@ -1,4 +1,5 @@
-import { Post } from "../../../../generated/prisma/client";
+import { Post, PostStatus } from "../../../../generated/prisma/client";
+import { PostWhereInput } from "../../../../generated/prisma/models";
 import { prisma } from "../../lib/prisma";
 
 const createPost = async (
@@ -15,8 +16,45 @@ const createPost = async (
   return post;
 };
 
-const getAllPost = async () => {
-  const posts = await prisma.post.findMany();
+const getAllPost = async ({
+  search,
+  tags,
+  isFeatured,
+  status,
+  authorId,
+}: {
+  search: string | undefined;
+  tags: string[] | undefined;
+  isFeatured: boolean;
+  status: PostStatus | undefined;
+  authorId: string | undefined;
+}) => {
+  const andConditions: PostWhereInput[] = [];
+
+  if (search) {
+    andConditions.push({
+      OR: [
+        { title: { contains: search, mode: "insensitive" } },
+        { content: { contains: search, mode: "insensitive" } },
+        { tags: { has: search } },
+      ],
+    });
+  }
+
+  if (tags && tags.length > 0) {
+    andConditions.push({ tags: { hasEvery: tags } });
+  }
+
+  if (isFeatured) andConditions.push({ isFeatured });
+  if (status) andConditions.push({ status });
+  if (authorId) andConditions.push({ authorId });
+
+  const whereConditions =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const posts = await prisma.post.findMany({
+    where: whereConditions,
+  });
   return posts;
 };
 
